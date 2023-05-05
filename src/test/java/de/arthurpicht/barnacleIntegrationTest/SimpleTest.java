e90157eb20521ed855676976b076fc08d444f430
Arthur Picht <picht@arthurpicht.de>
2023-05-05 09:12:51 +0200

    basic project setup

---
+++ src/test/java/de/arthurpicht/barnacleIntegrationTest/SimpleTest.java
@@ -0,0 +1,12 @@
+package de.arthurpicht.barnacleIntegrationTest;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void test() {
+        System.out.println("Hello world!");
+    }
+
+}
